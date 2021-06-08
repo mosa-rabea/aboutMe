847d443cebb2889593d6ad41f16e56459c90ace1
MmmmmNnnnn <>
2021-06-08 00:07:15 +0000

    aboutMe

---
+++ js/app.js
@@ -0,0 +1,84 @@
+'use strict';
+let userName=prompt('hi hi hi hi hi welcome ');
+alert('Welcome  ('+userName+') - i hope you will enjoy in this page - i will ask you about my self');
+
+let myName= prompt('my name is mousa?');
+if(myName.toLowerCase() ==='y' || myName.toLowerCase() ==='yes')
+  {
+    alert('sure this is my name ^^');
+  console.log('T');
+  
+} 
+ else if(myName.toLowerCase() ==='n' || myName.toLowerCase() ==='no')
+{
+  alert('why no look at the pic');
+  
+}
+ else{ alert(' you should answer by y or n'); }
+ 
+ 
+ 
+let favanime= prompt('death note is my best anime ?');
+if(favanime.toLowerCase() ==='y' || favanime.toLowerCase() ==='yes')
+  {
+  alert('i hope you watched it ');
+  
+} 
+else if(favanime.toLowerCase() ==='no' || favanime.toLowerCase() ==='n')
+{
+  alert('ooops');
+  
+}
+{
+
+console.log(' T');}
+
+{ alert(' you should answer by yes or no');}
+
+ 
+let favsport= prompt('My favorite sport is football?');
+if(favsport.toLowerCase() ==='yes' || favsport.toLowerCase() ==='y')
+{
+    alert('oops , no is not true  ');
+    
+    
+  } 
+  else if(favsport.toLowerCase() ==='no' || favsport.toLowerCase() ==='n')
+  { alert('this is clearly true');  }
+  console.log('T');
+ 
+  let favColor= prompt('White is My fav color?');
+  if(favColor.toLowerCase() ==='y' || favColor.toLowerCase() ==='yes')
+  {
+    alert('sure');
+  
+    
+  }
+   console.log(' T');
+    if(favColor.toLowerCase() ==='no' || favColor.toLowerCase() ==='n')
+  {
+    alert('oops ');
+    
+  }
+else{
+  alert('you must answer just y or n');
+}
+
+  let favName= prompt('mohammed is super best name i hear?');
+  if(favName.toLowerCase() ==='yes' || favName.toLowerCase() ==='y')
+  {
+    alert('عليه الصلاه والسلام ');
+    
+    
+}
+console.log('T');
+  if(favName.toLowerCase() ==='no' || favName.toLowerCase() ==='n')
+{
+  alert('why no , this is the greatest one in the world ');
+  
+}
+else{
+  alert('you should answer by yes or no');
+  
+}
+alert('nice to meet you ^_^ ${userName}' );
